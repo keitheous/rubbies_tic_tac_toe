8bca7b4fd35dd45749991e8e00e7be7b39c0dd53
Keith <kchong@zendesk.com>
2019-05-31 13:32:35 +1000

    Create Box class

--- lib/tic_tac_toe/box.rb
+++ lib/tic_tac_toe/box.rb
@@ -1,2 +1,12 @@
 class Box
+  attr_reader :position_x, :position_y, :value
+
+  def initialize(position_x, position_y)
+    @position_x = position_x
+    @position_y = position_y
+  end
+
+  def set_value(value)
+    @value = value
+  end
 end
--- spec/box_spec.rb
+++ spec/box_spec.rb
@@ -1,5 +1,24 @@
 require './lib/tic_tac_toe/box'
 
 RSpec.describe Box do
+  let(:box) { Box.new(1, 2) }
 
+  describe '.initialize' do
+    it 'returns 1 for position_x' do
+      expect(box.position_x).to eq(1)
+    end
+
+    it 'returns 2 as position_y' do
+      expect(box.position_y).to eq(2)
+    end
+  end
+
+  describe '#set_value' do
+    it 'sets attribute value' do
+      box.set_value('X')
+      expect(box.value).to eq('X')
+      box.set_value('O')
+      expect(box.value).to eq('O')
+    end
+  end
 end
